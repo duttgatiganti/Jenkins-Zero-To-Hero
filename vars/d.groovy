6e957ecede60fad8884a6b86926ac1c06c4b5c98
duttgatiganti <116521479+duttgatiganti@users.noreply.github.com>
2024-10-09 02:38:33 +0530

    Update d.groovy

--- vars/d.groovy
+++ vars/d.groovy
@@ -3,6 +3,6 @@ def doc() {
            
           // build the project and create a JAR file
            cd java-maven-sonar-argocd-helm-k8s/spring-boot-app
-        mvn clean package
+        mvn clean 
            
 }
