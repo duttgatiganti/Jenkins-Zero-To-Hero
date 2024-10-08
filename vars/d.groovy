76e52fc9ab983aba12b308eac86ceb3adc194d39
duttgatiganti <116521479+duttgatiganti@users.noreply.github.com>
2024-10-09 02:37:28 +0530

    Update d.groovy

--- vars/d.groovy
+++ vars/d.groovy
@@ -3,6 +3,6 @@ def doc() {
            
           // build the project and create a JAR file
            cd java-maven-sonar-argocd-helm-k8s/spring-boot-app
-         docker build -t dutt1/2app:v1 .
+        mvn clean package
            
 }
